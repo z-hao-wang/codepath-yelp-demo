050c8c68269060ebc900fe3debab5860e42b349d
haowang <haowang@zynga.com>
2015-02-10 14:26:17 -0800

    add basic ui and nav bar

--- Yelp/ViewController.swift
+++ Yelp/ViewController.swift
@@ -12,25 +12,31 @@ class ViewController: UIViewController {
     var client: YelpClient!
     
     // You can register for Yelp API keys here: http://www.yelp.com/developers/manage_api_keys
-    let yelpConsumerKey = "vxKwwcR_NMQ7WaEiQBK_CA"
-    let yelpConsumerSecret = "33QCvh5bIF5jIHR5klQr7RtBDhQ"
-    let yelpToken = "uRcRswHFYa1VkDrGV6LAW2F8clGh5JHV"
-    let yelpTokenSecret = "mqtKIxMIR4iBtBPZCmCLEb-Dz3Y"
+    let yelpConsumerKey = "pJ_jBXz1p2EMjnwZIeAmXA"
+    let yelpConsumerSecret = "mfiIYQ6KSKcrhhiQfT9lsQS1TXQ"
+    let yelpToken = "AYC_4RWA2pdnp01F0vWekL18SSR2RaI9"
+    let yelpTokenSecret = "SUam3twaVp35TEuT8x6aW05aGzc"
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        //init yelp client
+        client = YelpClient(consumerKey: yelpConsumerKey, consumerSecret: yelpConsumerSecret, accessToken: yelpToken, accessSecret: yelpTokenSecret)
+    }
+    
+    func searchTerm(term: String, location: String) {
+        client.searchWithTerm(term, location: location, success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
+            println(response)
+            }) { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
+                println(error)
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        client = YelpClient(consumerKey: yelpConsumerKey, consumerSecret: yelpConsumerSecret, accessToken: yelpToken, accessSecret: yelpTokenSecret)
+        println("view Did load")
+        searchTerm("Restaurant", location: "San Francisco")
         
-        client.searchWithTerm("Thai", success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
-            println(response)
-        }) { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
-            println(error)
-        }
     }
     
     override func didReceiveMemoryWarning() {
--- Yelp/YelpClient.swift
+++ Yelp/YelpClient.swift
@@ -26,9 +26,9 @@ class YelpClient: BDBOAuth1RequestOperationManager {
         self.requestSerializer.saveAccessToken(token)
     }
     
-    func searchWithTerm(term: String, success: (AFHTTPRequestOperation!, AnyObject!) -> Void, failure: (AFHTTPRequestOperation!, NSError!) -> Void) -> AFHTTPRequestOperation! {
+    func searchWithTerm(term: String, location: String, success: (AFHTTPRequestOperation!, AnyObject!) -> Void, failure: (AFHTTPRequestOperation!, NSError!) -> Void) -> AFHTTPRequestOperation! {
         // For additional parameters, see http://www.yelp.com/developers/documentation/v2/search_api
-        var parameters = ["term": term, "location": "San Francisco"]
+        var parameters = ["term": term, "location": location]
         return self.GET("search", parameters: parameters, success: success, failure: failure)
     }
     
